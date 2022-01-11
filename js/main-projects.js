a3ce520d0da7e4b9b35e2fb3246901d9398407ba
김용일 <kyi910705@gmail.com>
2022-01-12 00:31:08 +0900

    프로젝트 추가

--- js/main-projects.js
+++ js/main-projects.js
@@ -2,34 +2,160 @@ const mainProjects = [
     {
         title: '디지털교과서 펜쓰기 모듈 고도화 개발',
         date: '2021.11.01 - 2021.12.31',
-        company: 'KERIS',
-        thumbnail: 'images/projects/kerisDraw/1.jpg',
+        company: '한국교육학술정보원',
+        thumbnail: 'images/projects/keris-draw/4.png',
         images: [
-            'images/projects/kerisDraw/1.jpg',
-            'images/projects/kerisDraw/2.jpg',
+            'images/projects/keris-draw/1.png',
+            'images/projects/keris-draw/2.png',
+            'images/projects/keris-draw/3.png',
+            'images/projects/keris-draw/4.png',
         ],
     },
     {
         title: '디지털교과서 노트 기능 웹 포팅 개발',
-        date: '2021.11.01 - 2021.12.31',
-        company: 'KERIS',
-        thumbnail: 'images/projects/kerisDraw/3.png',
+        date: '2021.08.01 - 2021.10.31',
+        company: '한국교육학술정보원',
+        thumbnail: 'images/projects/keris-note/8.png',
         images: [
-            'images/projects/kerisDraw/1.jpg',
-            'images/projects/kerisDraw/2.jpg',
+            'images/projects/keris-note/1.png',
+            'images/projects/keris-note/2.png',
+            'images/projects/keris-note/3.png',
+            'images/projects/keris-note/4.png',
+            'images/projects/keris-note/5.png',
+            'images/projects/keris-note/6.png',
+            'images/projects/keris-note/7.png',
+            'images/projects/keris-note/8.png',
+            'images/projects/keris-note/9.png',
+            'images/projects/keris-note/10.png',
+            'images/projects/keris-note/11.png',
         ],
     },
     {
-        title: '가족지킴이 iOS 15 버전 업데이트 이슈 대응 개발',
-        date: '2021.11.01 - 2021.12.31',
-        company: 'KERIS',
-        thumbnail: 'images/projects/kerisDraw/4.png',
+        title: '가족지킴이 iOS 15 버전 이슈 대응 개발',
+        date: '2021.07.01 - 2021.07.31',
+        company: 'LG 유플러스',
+        thumbnail: 'images/projects/lgu-kidwatch-issue/2.png',
         images: [
-            'images/projects/kerisDraw/1.jpg',
-            'images/projects/kerisDraw/2.jpg',
+            'images/projects/lgu-kidwatch-issue/1.png',
+            'images/projects/lgu-kidwatch-issue/2.png',
+        ],
+    },
+    {
+        title: '중·고등 영어 전자저작물 뷰어 개발',
+        date: '2021.03.01 - 2021.06.30',
+        company: '비상교육',
+        thumbnail: 'images/projects/visang-english/1.png',
+        images: [
+            'images/projects/visang-english/1.png',
+            'images/projects/visang-english/2.png',
+            'images/projects/visang-english/3.png',
+            'images/projects/visang-english/4.png',
+            'images/projects/visang-english/5.png',
+            'images/projects/visang-english/6.png',
+            'images/projects/visang-english/7.png',
+            'images/projects/visang-english/8.png',
+        ],
+    },
+    {
+        title: '디지털교과서 화면 개선 및 기능 개발',
+        date: '2021.02.08 - 2021.02.28',
+        company: '한국교육학술정보원',
+        thumbnail: 'images/projects//1.png',
+        images: [
+            'images/projects/visang-english/1.png',
+            'images/projects/visang-english/2.png',
+            'images/projects/visang-english/3.png',
+            'images/projects/visang-english/4.png',
+            'images/projects/visang-english/5.png',
+            'images/projects/visang-english/6.png',
+            'images/projects/visang-english/7.png',
+            'images/projects/visang-english/8.png',
+        ],
+    },
+    {
+        title: 'Y클라우드 교과서 컨텐츠 웹 버전 뷰어 개발',
+        date: '2021.01.18 - 2021.02.18',
+        company: 'YBM',
+        thumbnail: 'images/projects//1.png',
+        images: [
+            'images/projects/visang-english/1.png',
+            'images/projects/visang-english/2.png',
+            'images/projects/visang-english/3.png',
+            'images/projects/visang-english/4.png',
+            'images/projects/visang-english/5.png',
+            'images/projects/visang-english/6.png',
+            'images/projects/visang-english/7.png',
+            'images/projects/visang-english/8.png',
+        ],
+    },
+    {
+        title: '신분증, 신용카드 React-Native SDK 포팅',
+        date: '2020.11.01 - 2020.11.30',
+        company: '베리타스자산대부',
+        thumbnail: 'images/projects//1.png',
+        images: [
+            'images/projects/visang-english/1.png',
+            'images/projects/visang-english/2.png',
+            'images/projects/visang-english/3.png',
+            'images/projects/visang-english/4.png',
+            'images/projects/visang-english/5.png',
+            'images/projects/visang-english/6.png',
+            'images/projects/visang-english/7.png',
+            'images/projects/visang-english/8.png',
+        ],
+    },
+    {
+        title: '법원문서 OCR 뷰어 및 관리자 페이지 개발',
+        date: '2020.08.01 - 2020.11.30',
+        company: '베리타스자산대부',
+        thumbnail: 'images/projects//1.png',
+        images: [
+            'images/projects/visang-english/1.png',
+            'images/projects/visang-english/2.png',
+            'images/projects/visang-english/3.png',
+            'images/projects/visang-english/4.png',
+            'images/projects/visang-english/5.png',
+            'images/projects/visang-english/6.png',
+            'images/projects/visang-english/7.png',
+            'images/projects/visang-english/8.png',
         ],
     },
 ]
+
+/* 
+<div>- <span class="text-primary">베리타스자산대부</span>&nbsp;&nbsp;관리자 페이지, OCR 교정 뷰어 개발</div>
+<div>- <span class="text-primary">현대차 모션</span>&nbsp;&nbsp;신분증, 신용카드 OCR React-Native 포팅 개발</div>
+<div>- <span class="text-primary">롯데e커머스</span>&nbsp;&nbsp;OCR 엔진 연동 및 서비스 API 개발</div>
+<div>- <span class="text-primary">SK 스토아</span>&nbsp;&nbsp;OCR 엔진 유지보수 이슈 대응 개발</div>
+<div>- <span class="text-primary">KB 국민은행</span>&nbsp;&nbsp;클라이언트 API 개발 및 관리자 페이지 개발</div>
+</div>
+</p>
+<span class="position text-gray">영상지능팀 - 매니저</span>
+<p class="text-dark">
+<div>
+<div>- <span class="text-primary">LG 유플러스</span>&nbsp;&nbsp;5G 서비스 위젯 관리자 페이지 개발</div>
+<div>- <span class="text-primary">LG 유플러스</span>&nbsp;&nbsp;AI 스피커 매니저앱 서비스 API 서버 개발</div>
+<div>- <span class="text-primary">LG 유플러스</span>&nbsp;&nbsp;라디오 스트리밍 데이터 실시간 코덱 변환 모듈 개발</div>
+<div>- <span class="text-primary">LG 유플러스</span>&nbsp;&nbsp;iOS 가족지킴이 앱 이슈 대응 개발</div>
+</div>
+</p>
+</div>
+</div>
+<div class="resume-wrap d-flex ftco-animate">
+<!-- <div class="icon d-flex align-items-center justify-content-center">
+<span class="flaticon-ideas"></span>
+</div> -->
+<div class="text">
+<span class="date">2014.12 - 2018.02</span>
+<h2>솔루비스</h2>
+<span class="position text-gray">솔루션개발팀 - 대리</span>
+<p class="text-dark">
+<div>
+<div>- <span class="text-primary">BS렌탈</span>&nbsp;&nbsp;상담원 소프트폰 프로그램 개발</div>
+<div>- <span class="text-primary">ABL생명</span>&nbsp;&nbsp;고객 화상 상담 클라이언트 프로그램 개발</div>
+<div>- <span class="text-primary">알리안츠생명</span>&nbsp;&nbsp;모바일 비대면 상담 클라이언트 프로그램 개발</div>
+<div>- <span class="text-primary">경남은행</span>&nbsp;&nbsp;모바일 비대면 상담 클라이언트 프로그램 개발</div>
+</div> */
 
 function loadMainProejcts() {
 
@@ -41,7 +167,7 @@ function loadMainProejcts() {
         const template = `
             <div class="col-md-4 d-flex ftco-animate fadeInUp ftco-animated">
                 <div class="blog-entry justify-content-end">
-                    <img class="block-20" src="${project.thumbnail}" style="width: 100%; height: 200px; object-fit: contain;" />
+                    <img class="block-20 border" src="${project.thumbnail}" style="width: 100%; height: 200px; object-fit: cover;" />
                     <div class="text mt-3 float-right d-block">                        
                         <span class="text-dark font-weight-bold text-omission mb-0" style="width: ${imageWidth}px;" title="${project.title}">
                             ${project.title}
@@ -63,4 +189,8 @@ function loadMainProejcts() {
 
 $(function() {
     loadMainProejcts();
+
+    window.addEventListener('resize', () => {
+        loadMainProejcts();
+    })
 });
